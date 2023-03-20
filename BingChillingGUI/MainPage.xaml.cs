3bd0b362e8f1c51de652a2e60bf1d3e9dc55911c
arieljovananda88 <jovanandaa@gmail.com>
2023-03-20 17:52:19 +0700

    changes to xaml.cs, but still needs changes

--- BingChillingGUI/MainPage.xaml.cs
+++ BingChillingGUI/MainPage.xaml.cs
@@ -1,36 +1,112 @@
-﻿namespace BingChillingGUI;
-
-public partial class MainPage : ContentPage
+﻿namespace BingChillingGUI
 {
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-    private async void Button_Clicked(System.Object sender, System.EventArgs e)
+    public partial class MainPage : ContentPage
     {
-        PickOptions options = new PickOptions();
-        try
+        public MainPage()
         {
-            var result = await FilePicker.Default.PickAsync(options);
-            if (result != null)
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            PickOptions options = new PickOptions();
+            try
             {
-                if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
-                    result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                var result = await FilePicker.Default.PickAsync(options);
+                if (result != null)
                 {
-                    using var stream = await result.OpenReadAsync();
-                    var image = ImageSource.FromStream(() => stream);
+                    if (result.FileName.EndsWith("txt", StringComparison.OrdinalIgnoreCase))
+                    {
+                        using var stream = await result.OpenReadAsync();
+                        using var reader = new StreamReader(stream);
+                        int row = 0, col = 0;
+                        int[,] matrix = new int[5, 5]; // Fixed dimensions for the example maze
+
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            col = 0;
+                            foreach (char c in line)
+                            {
+                                if (c == 'X')
+                                    matrix[row, col] = -1; // Wall
+                                else if (c == 'R')
+                                    matrix[row, col] = 1; // Road
+                                else if (c == 'T')
+                                    matrix[row, col] = 2; // Treasure
+                                col++;
+                            }
+                            row++;
+                        }
+
+                        DisplayMaze(matrix);
+                    }
+                }
+
+            }
+            catch (Exception ex) //
+            {
+                // The user canceled or something went wrong
+            }
+        }
+
+        private void DisplayMaze(int[,] matrix)
+        {
+            // Create a new grid to hold the maze
+            var grid = new Grid
+            {
+                ColumnSpacing = 0,
+                RowSpacing = 0
+            };
+
+            // Add columns and rows to the grid
+            for (int i = 0; i < 5; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+                grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+            }
+
+            // Loop through the matrix and create a label for each element
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    // Create a new label for this element
+                    var label = new Label
+                    {
+                        Text = matrix[i, j] == 2 ? "Treasure" : "",
+                        TextColor = matrix[i, j] == 2 ? Color.FromRgb(0, 0, 0) : Color.FromRgb(255, 255, 255),
+                        BackgroundColor = matrix[i, j] == -1 ? Color.FromRgb(0, 0, 0) : Color.FromRgb(255, 255, 255),
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        VerticalTextAlignment = TextAlignment.Center,
+                    };
+
+                    // Add the label to the grid
+                    grid.Children.Add(label);
                 }
             }
 
+            // Add the grid to the main page
+            Content = grid;
         }
-        catch (Exception ex)
+
+
+        // Helper method to get the background color for each element
+        private Color GetColorForElement(int element)
         {
-            // The user canceled or something went wrong
+            Color gray = Color.FromRgb(128, 128, 128);
+            switch (element)
+            {
+                case -1: // Wall
+                    return Color.FromRgb(128, 128, 128);
+                case 1: // Road
+                    return Color.FromRgb(255, 255, 255);
+                case 2: // Treasure
+                    return Color.FromRgb(255, 255, 0);
+                default: // Unknown element
+                    return Color.FromRgb(0, 0, 0);
+            }
         }
+
     }
 }
-
-
